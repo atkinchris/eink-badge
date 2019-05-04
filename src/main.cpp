e8c009c8554920187e50aca672f888ba94690cf7
Chris Atkin <atkinchris@gmail.com>
2019-05-04 19:55:11 +0100

    Write message in response to POST message

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,93 @@
 #include <GxEPD.h>
 #include <GxGDE0213B72/GxGDE0213B72.h>
-#include <Fonts/FreeMono9pt7b.h>
 #include <GxIO/GxIO_SPI/GxIO_SPI.h>
 #include <GxIO/GxIO.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ESPmDNS.h>
+
+#include <Fonts/FreeMono9pt7b.h>
+#include <Fonts/FreeSansBold9pt7b.h>
 
 #include "pinout.h"
 
-// GxIO_SPI(SPIClass& spi, int8_t cs, int8_t dc, int8_t rst = -1, int8_t bl = -1);
-GxIO_Class io(SPI, ELINK_SS, ELINK_DC, ELINK_RESET);
-// GxGDEP015OC1(GxIO& io, uint8_t rst = D4, uint8_t busy = D2);
-GxEPD_Class display(io, ELINK_RESET, ELINK_BUSY);
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
 
-void setup() {
-  int16_t x, y, x1, y1;
+GxIO_Class io(SPI, ELINK_SS, ELINK_DC, ELINK_RESET);
+GxEPD_Class display(io, ELINK_RESET, ELINK_BUSY);
+AsyncWebServer server(80);
+char message[32];
+
+String connectToWifi()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    esp_restart();
+  }
+
+  Serial.println(F("WiFi connected"));
+  Serial.println(WiFi.localIP());
+  return WiFi.localIP().toString();
+}
+
+void printTextToString(String text, const GFXfont *font)
+{
+  int16_t x1, y1;
   uint16_t w, h;
+
+  display.setFont(font);
+  display.fillScreen(GxEPD_WHITE);
+  display.getTextBounds(text, 0, 0, &x1, &y1, &w, &h);
+  display.setCursor(display.width() / 2 - ((w + x1) / 2), display.height() / 2 - ((h + y1) / 2));
+  display.println(text);
+  display.update();
+}
+
+void startServer()
+{
+  if (MDNS.begin("ttgo"))
+  {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/message", HTTP_POST, [](AsyncWebServerRequest *request) {
+    for (int i = 0; i < request->params(); i++)
+    {
+      String key = request->getParam(i)->name();
+      String value = request->getParam(i)->value();
+      Serial.println(key + " : " + value);
+
+      if (key == "message")
+      {
+        strlcpy(message, value.c_str(), sizeof(message));
+      }
+      else if (key == "fontSize")
+      {
+        display.setTextSize(value.toInt());
+      }
+    }
+
+    printTextToString(message, &FreeSansBold9pt7b);
+    request->send(200, "text/plain", message);
+  });
+
+  server.onNotFound([](AsyncWebServerRequest *request) {
+    request->send(404, "text/plain", "Not found");
+  });
+
+  MDNS.addService("http", "tcp", 80);
+
+  server.begin();
+}
+
+void setup()
+{
   Serial.begin(115200);
   delay(500);
 
@@ -22,27 +96,16 @@ void setup() {
   display.eraseDisplay();
   display.setTextColor(GxEPD_BLACK);
   display.setFont(&FreeMono9pt7b);
-
   display.setRotation(1);
-  display.fillScreen(GxEPD_WHITE);
-  display.update();
+  display.setTextSize(1);
 
-  x = 0;
-  y = 50;
-  display.setTextSize(2);
-  String str = "Hello,";
-  display.setCursor(x, y);
-  display.getTextBounds(str, x, y, &x1, &y1, &w, &h);
-  display.setCursor(display.width() / 2 - ((w + x1) / 2), display.height() / 2 - ((h + y1) / 2));
-  display.println(str);
+  String ipAddress = connectToWifi();
+  startServer();
 
-  y = display.getCursorY() + 1;
-  display.setCursor(0, y);
-  display.println("I am Chris!");
-
-  display.update();
+  printTextToString(ipAddress, &FreeMono9pt7b);
 }
 
-void loop() {
+void loop()
+{
   delay(1000);
 }
